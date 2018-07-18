c78171e0c7d888c8d6d7a8738a4f5c7ebcff4c83
storm37000 <darkrai37@hotmail.com>
2018-07-17 21:31:17 -0400

    initial commit

---
+++ Default.cs
@@ -0,0 +1,64 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.Events;
+using Smod2.EventHandlers;
+
+namespace Smod.TestPlugin
+{
+    [PluginDetails(
+        author = "storm37000",
+        name = "StayShut",
+        description = "Keeps doors shut when nuke countdown starts",
+        id = "s37k.stayshut",
+        version = "1.0.0",
+        SmodMajor = 3,
+        SmodMinor = 1,
+        SmodRevision = 0
+        )]
+    class Default : Plugin
+    {
+        public override void OnDisable()
+        {
+            this.Info(this.Details.name + " has been disabled.");
+        }
+        public override void OnEnable()
+        {
+            bool SSLerr = false;
+            this.Info(this.Details.name + " has been enabled.");
+            while (true)
+            {
+                try
+                {
+                    string host = "https://storm37000.tk/addons/";
+                    if (SSLerr) { host = "http://74.91.115.126/addons/"; }
+                    ushort version = ushort.Parse(this.Details.version.Replace(".", string.Empty));
+                    ushort fileContentV = ushort.Parse(new System.Net.WebClient().DownloadString(host + this.Details.name + ".ver"));
+                    if (fileContentV > version)
+                    {
+                        this.Info("Your version is out of date, please visit the Smod discord and download the newest version");
+                    }
+                    break;
+                }
+                catch (System.Exception e)
+                {
+                    if (SSLerr == false && e.Message.Contains("The authentication or decryption has failed."))
+                    {
+                        SSLerr = true;
+                        continue;
+                    }
+                    this.Error("Could not fetch latest version txt: " + e.Message);
+                    break;
+                }
+            }
+        }
+
+        public override void Register()
+        {
+            // Register Events
+            this.AddEventHandler(typeof(IEventHandlerRoundStart), new EventHandler(this), Priority.Highest);
+            this.AddEventHandler(typeof(IEventHandlerWarheadDetonate), new EventHandler(this), Priority.Highest);
+            this.AddEventHandler(typeof(IEventHandlerWarheadStartCountdown), new EventHandler(this), Priority.Highest);
+            this.AddEventHandler(typeof(IEventHandlerWarheadStopCountdown), new EventHandler(this), Priority.Highest);
+        }
+    }
+}
---
+++ EventHandler.cs
@@ -0,0 +1,75 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.Events;
+using Smod2.EventHandlers;
+
+namespace Smod.TestPlugin
+{
+    class EventHandler : IEventHandlerRoundStart, IEventHandlerWarheadDetonate, IEventHandlerWarheadStartCountdown, IEventHandlerWarheadStopCountdown
+    {
+        private Plugin plugin;
+
+        public EventHandler(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        private System.Timers.Timer ulck = new System.Timers.Timer();
+        private void InitTimer(uint time)
+        {
+            ulck.Interval = time * 1000;
+            ulck.AutoReset = false;
+            ulck.Enabled = true;
+            ulck.Elapsed += delegate
+            {
+                foreach (Smod2.API.Door door in PluginManager.Manager.Server.Map.GetDoors())
+                {
+                    door.Locked = false;
+                }
+                ulck.Enabled = false;
+                plugin.Info("unlocking doors");
+            };
+        }
+
+        public void OnDetonate()
+        {
+            ulck.Enabled = false;
+            foreach (Smod2.API.Door door in PluginManager.Manager.Server.Map.GetDoors())
+            {
+                if(door.Position.y <= 900)
+                {
+                    door.Destroyed = true;
+                }
+            }
+            foreach (int ind in System.Enum.GetValues(typeof(ItemType)))
+            {
+                if (ind == -1) { continue; }
+                foreach (Item item in PluginManager.Manager.Server.Map.GetItems(((ItemType)ind), false))
+                {
+                    if(item.GetPosition().y <= 900)
+                    {
+                        item.Remove();
+                    }
+                }
+            }
+        }
+
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+            foreach (Smod2.API.Door door in ev.Server.Map.GetDoors())
+            {
+                door.DontOpenOnWarhead = true;
+            }
+        }
+
+        public void OnStartCountdown(WarheadStartEvent ev)
+        {
+            InitTimer(2);
+        }
+
+        public void OnStopCountdown(WarheadStopEvent ev)
+        {
+            ulck.Enabled = false;
+        }
+    }
+}
