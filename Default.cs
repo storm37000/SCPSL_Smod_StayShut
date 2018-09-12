6bb1d3f1e2c108506ae8395b86e78853613ff932
storm37000 <6331913+storm37000@users.noreply.github.com>
2018-09-11 22:59:01 -0400

    more noticable warning

--- Default.cs
+++ Default.cs
@@ -10,7 +10,7 @@ namespace Smod.TestPlugin
         name = "StayShut",
         description = "Keeps doors shut when nuke countdown starts",
         id = "s37k.stayshut",
-        version = "1.0.1",
+        version = "1.0.2",
         SmodMajor = 3,
         SmodMinor = 1,
         SmodRevision = 0
@@ -63,6 +63,10 @@ namespace Smod.TestPlugin
             this.AddConfig(new Smod2.Config.ConfigSetting("ss_nuke_destroy_doors", false, Smod2.Config.SettingType.BOOL, true, ""));
 //            this.AddConfig(new Smod2.Config.ConfigSetting("ss_nuke_destroy_items", true, Smod2.Config.SettingType.BOOL, true, ""));
             this.AddConfig(new Smod2.Config.ConfigSetting("ss_doors_stay_shut", true, Smod2.Config.SettingType.BOOL, true, ""));
+            if (ConfigManager.Manager.Config.GetBoolValue("lock_gates_on_countdown", true))
+            {
+                this.Error("lock_gates_on_countdown MUST be set to false for this plugin to be able to work safely!");
+            }
         }
     }
 }
